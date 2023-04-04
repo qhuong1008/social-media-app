a2b927a28e55b37a00509661cb37c3734da4f8ce
qhuong1008 <qhuong1008@gmail.com>
2023-04-04 21:10:33 +0700

    modal fix

--- src/components/FollowerModal/FollowerModal.jsx
+++ src/components/FollowerModal/FollowerModal.jsx
@@ -6,85 +6,17 @@ function FollowerModal() {
       <div className="ruler"></div>
       <div className="follower-list">
         <div className="follower-item">
-          <a href="/profile/catlmao1234">
-            <div className="follower-left">
-              <img
-                src="https://i.pinimg.com/474x/ee/2a/71/ee2a7149341c2b23ae2e9c7358ec247d.jpg"
-                alt=""
-              />
+          <div className="follower-left">
+            <img
+              src="https://i.pinimg.com/474x/ee/2a/71/ee2a7149341c2b23ae2e9c7358ec247d.jpg"
+              alt=""
+            />
+            <a href="/profile/catlmao1234">
               <div className="username">catlmao1234</div>
-            </div>
-          </a>
-          <div className="follower-right">
-            <div className="delete-btn">Remove</div>
+            </a>
+            <div className="follow-btn">Follow</div>
           </div>
-        </div>
-        <div className="follower-item">
-          <a href="/profile/catlmao1234">
-            <div className="follower-left">
-              <img
-                src="https://i.pinimg.com/474x/ee/2a/71/ee2a7149341c2b23ae2e9c7358ec247d.jpg"
-                alt=""
-              />
-              <div className="username">catlmao1234</div>
-            </div>
-          </a>
-          <div className="follower-right">
-            <div className="delete-btn">Remove</div>
-          </div>
-        </div>
-        <div className="follower-item">
-          <a href="/profile/catlmao1234">
-            <div className="follower-left">
-              <img
-                src="https://i.pinimg.com/474x/ee/2a/71/ee2a7149341c2b23ae2e9c7358ec247d.jpg"
-                alt=""
-              />
-              <div className="username">catlmao1234</div>
-            </div>
-          </a>
-          <div className="follower-right">
-            <div className="delete-btn">Remove</div>
-          </div>
-        </div>
-        <div className="follower-item">
-          <a href="/profile/catlmao1234">
-            <div className="follower-left">
-              <img
-                src="https://i.pinimg.com/474x/ee/2a/71/ee2a7149341c2b23ae2e9c7358ec247d.jpg"
-                alt=""
-              />
-              <div className="username">catlmao1234</div>
-            </div>
-          </a>
-          <div className="follower-right">
-            <div className="delete-btn">Remove</div>
-          </div>
-        </div>
-        <div className="follower-item">
-          <a href="/profile/catlmao1234">
-            <div className="follower-left">
-              <img
-                src="https://i.pinimg.com/474x/ee/2a/71/ee2a7149341c2b23ae2e9c7358ec247d.jpg"
-                alt=""
-              />
-              <div className="username">catlmao1234</div>
-            </div>
-          </a>
-          <div className="follower-right">
-            <div className="delete-btn">Remove</div>
-          </div>
-        </div>
-        <div className="follower-item">
-          <a href="/profile/catlmao1234">
-            <div className="follower-left">
-              <img
-                src="https://i.pinimg.com/474x/ee/2a/71/ee2a7149341c2b23ae2e9c7358ec247d.jpg"
-                alt=""
-              />
-              <div className="username">catlmao1234</div>
-            </div>
-          </a>
+
           <div className="follower-right">
             <div className="delete-btn">Remove</div>
           </div>
