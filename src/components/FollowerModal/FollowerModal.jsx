7e1863472aa8b1770da602a4cf37c1ce402dd03e
baongoc02 <tranthibaongoc779152@gmail.com>
2023-04-06 12:04:13 +0700

    UPDATE: followingModal and followerModal
    - load list followers and following

--- src/components/FollowerModal/FollowerModal.jsx
+++ src/components/FollowerModal/FollowerModal.jsx
@@ -1,28 +1,62 @@
+import { useEffect } from "react";
 import styles from "./FollowerModal.scss";
+import { CommonFollowerApi } from "../../api/common";
+import { useState } from "react";
+import { Table } from "antd";
 function FollowerModal() {
+  const [listFollowers, setListFollowers] = useState([]);
+
+  function fetchListFollowers() {
+    return CommonFollowerApi.listFollowers()
+    .then ((response) => {
+        const fetchListFollowers = response.data.data.map((follower, index) => {
+            const temp = { ...follower };
+            return temp;
+        });
+        setListFollowers(fetchListFollowers);
+    })
+    .catch((error) => {});
+  }
+  useEffect (()=> {
+    fetchListFollowers();
+  }, []);
+
   return (
-    <div className="follower-modal-wrapper">
+    <>
+        <div className="follower-modal-wrapper">
       <div className="follower-modal-header">Followers</div>
       <div className="ruler"></div>
       <div className="follower-list">
+        
+    {Object.keys(listFollowers).map(key => {
+      return (
         <div className="follower-item">
           <div className="follower-left">
             <img
-              src="https://i.pinimg.com/474x/ee/2a/71/ee2a7149341c2b23ae2e9c7358ec247d.jpg"
+              // src="https://i.pinimg.com/474x/ee/2a/71/ee2a7149341c2b23ae2e9c7358ec247d.jpg"
+              src={listFollowers[key].avatar}
+
               alt=""
             />
             <a href="/profile/catlmao1234">
-              <div className="username">catlmao1234</div>
+              {/* <div className="username">catlmao1234</div> */}
+              <div className="username">{listFollowers[key].username}</div>
             </a>
             <div className="follow-btn">Follow</div>
           </div>
+
+          
 
           <div className="follower-right">
             <div className="delete-btn">Remove</div>
           </div>
         </div>
+      )
+    })}
+        
       </div>
     </div>
+    </>
   );
 }
 
--- src/components/FollowingModal/FollowingModal.jsx
+++ src/components/FollowingModal/FollowingModal.jsx
@@ -1,18 +1,46 @@
 import styles from "./FollowingModal.scss";
+import { useState, useEffect } from "react";
+import { CommonFollowerApi } from "../../api/common";
+
 function FollowingModal() {
+  const [listFollowing, setListFollowing] = useState([]);
+
+  function fetchListFollowing(){
+    CommonFollowerApi.listFollowing()
+    .then((res) => {
+      const fetchListFollowing = res.data.data.map((following, index) => {
+        const temp = { ...following };
+        return temp;
+      })
+      setListFollowing(fetchListFollowing);
+    })
+    .catch((err) => {});
+  }
+
+  
+  useEffect (() => {
+    fetchListFollowing();
+  }, []);
   return (
+    <>
+
     <div className="follower-modal-wrapper">
-      <div className="follower-modal-header">Followers</div>
+      <div className="follower-modal-header">Followeing</div>
       <div className="ruler"></div>
       <div className="follower-list">
+      {Object.keys(listFollowing).map(key => {
+      return (
+        
         <div className="follower-item">
           <div className="follower-left">
             <img
-              src="https://i.pinimg.com/474x/ee/2a/71/ee2a7149341c2b23ae2e9c7358ec247d.jpg"
+              // src="https://i.pinimg.com/474x/ee/2a/71/ee2a7149341c2b23ae2e9c7358ec247d.jpg"
+              src = {listFollowing[key].avatar}
               alt=""
             />
             <a href="/profile/catlmao1234">
-              <div className="username">catlmao1234</div>
+              <div className="username">{listFollowing[key].username}</div>
+              {/* <div className="username">catlmao1234</div> */}
             </a>
             <div className="follow-btn">Follow</div>
           </div>
@@ -20,9 +48,14 @@ function FollowingModal() {
           <div className="follower-right">
             <div className="delete-btn">Remove</div>
           </div>
-        </div>
+          </div>
+          
+    )
+  })}
+        
       </div>
     </div>
+    </>
   );
 }
 
