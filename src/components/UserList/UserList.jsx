e807386cf9a9447f2c46d020828f00f15a62704a
qhuong1008 <qhuong1008@gmail.com>
2023-05-04 14:43:03 +0700

    load user suggestions

--- src/components/UserList/UserList.jsx
+++ src/components/UserList/UserList.jsx
@@ -1,11 +1,65 @@
 import style from "./UserList.scss";
 import avatar from "../../assets/img/avt.jpg";
+import { useState, useEffect } from 'react'
+import { CommonUserApi } from "../../api/common";
+import { USER_KEY_NAME } from "../../types";
 
 function UserList() {
   const user = JSON.parse(localStorage.getItem("USER_INFO"));
 
   if (user === null) {
     window.location.href = "/login";
+  }
+  const [userList, setUserList] = useState([])
+  const [selectedReceiverJSONInfo, setSelectedReceiverJSONInfo] =
+    useState(null);
+  const [listFollowers, setListFollowers] = useState([]);
+  const [userSearchResult, setUserSearchresult] = useState([])
+  const [searchInput, setSearchInput] = useState('')
+
+  const getCurrentLogedInUser = () => {
+    if (localStorage.getItem(USER_KEY_NAME) == null) return null;
+    try {
+      const user = JSON.parse(localStorage.getItem(USER_KEY_NAME));
+      return user;
+    } catch (e) {
+      return null;
+    }
+  };
+
+  /**
+   * Thông tin user đang đăng nhập hiện tại:
+   *   - **id**: id người dùng
+   *   - **username**: tên đăng nhập
+   *   - **displayName**: tên hiển thị ra ngoài
+   */
+  const currentLoginedUser = getCurrentLogedInUser();
+
+  const handleSearchForUsers = (e) => {
+    setSearchInput(e.target.value)
+    const resultList = userList.filter(user => {
+      return user.username.includes(e.target.value)
+    })
+    setUserSearchresult(resultList)
+  }
+
+  useEffect(() => {
+    CommonUserApi.listUsers().then((res) => {
+      const listUserExceptMe = res.data.data.filter(
+        (user) => user.id != currentLoginedUser.id
+      );
+
+      setUserList(listUserExceptMe);
+      setUserSearchresult(listUserExceptMe)
+      if (listUserExceptMe.length > 0) {
+        setSelectedReceiverJSONInfo(listUserExceptMe[0]);
+      }
+    });
+  }, []);
+
+  console.log(userList[0])
+  if (userList.length === 0) {
+    return <>loading</>
   }
 
   return (
@@ -36,50 +90,39 @@ function UserList() {
           <div className="suggest">Suggestions for you</div>
           <div className="all">See all</div>
         </div>
-        <ul className="account-list">
-          <li className="account-item">
-            <div className="user-account">
-              <div className="bottom-userlist-container">
-                <div className="user-main">
-                  <div className="avt">
-                    <img
-                      src="https://i.pinimg.com/474x/65/90/f7/6590f7a352330539d159602b1588dffc.jpg"
-                      alt="avatar"
-                    />
-                  </div>
-                  <div className="name">
-                    <div className="username">user</div>
-                    <div className="full-name">user</div>
-                  </div>
-                </div>
-                <div className="userlist-action">
-                  <a href="">Switch</a>
-                </div>
-              </div>
-            </div>
-          </li>
-          <li className="account-item">
-            <div className="user-account">
-              <div className="bottom-userlist-container">
-                <div className="user-main">
-                  <div className="avt">
-                    <img
-                      src="https://i.pinimg.com/474x/65/90/f7/6590f7a352330539d159602b1588dffc.jpg"
-                      alt="avatar"
-                    />
-                  </div>
-                  <div className="name">
-                    <div className="username">koyuki_chan01</div>
-                    <div className="full-name">Koyuki Chan</div>
-                  </div>
-                </div>
-                <div className="userlist-action">
-                  <a href="">Follow</a>
-                </div>
-              </div>
-            </div>
-          </li>
-        </ul>
+        {userList.length > 2 && <ul className="account-list">
+          {
+            userList.map((user, index) => {
+              if (index < 3) {
+                return (
+                  <li className="account-item">
+                    <div className="user-account">
+                      <div className="bottom-userlist-container">
+                        <div className="user-main">
+                          <div className="avt">
+                            <img
+                              src={user.avatar}
+                            />
+                          </div>
+                          <div className="name">
+                            <div className="username">{user.username}</div>
+                            <div className="full-name">{user.displayName}</div>
+                          </div>
+                        </div>
+                        <div className="userlist-action">
+                          <a href={`/profile/${user.username}`}>View Profile</a>
+                        </div>
+                      </div>
+                    </div>
+                  </li>
+
+                )
+              }
+            })
+          }
+
+
+        </ul>}
       </div>
     </div>
   );
