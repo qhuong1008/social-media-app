4a7c01ccc2f03f186f5cc5a1fe43c99152af00ca
Nguyễn Thành Văn <20110224@student.hcmute.edu.vn>
2023-05-09 09:40:37 +0700

    FEAT: Notification

---
+++ src/components/Notification/Notification.jsx
@@ -0,0 +1,39 @@
+import { useStompClient, useSubscription } from "react-stomp-hooks"
+import { handleSuccessMessage } from "../../api/toast";
+import { USER_KEY_NAME } from "../../types";
+
+const Notification = (props) => {
+    /**
+     * Lấy thông tin user đang đăng nhập hiện tại trong localStorage
+     * @returns user JSON
+     */
+    const getCurrentLogedInUser = () => {
+        if (localStorage.getItem(USER_KEY_NAME) == null) return null;
+        try {
+            const user = JSON.parse(localStorage.getItem(USER_KEY_NAME));
+            return user;
+        } catch (e) {
+            return null;
+        }
+    };
+
+    /**
+     * Thông tin user đang đăng nhập hiện tại:
+     *   - **id**: id người dùng
+     *   - **username**: tên đăng nhập
+     *   - **displayName**: tên hiển thị ra ngoài
+     */
+    const currentLoginedUser = getCurrentLogedInUser();
+
+    useSubscription('/ws/secured/announce/user-' + currentLoginedUser.id, (message) => {
+        const bodyJson = JSON.parse(message.body);
+        handleSuccessMessage(bodyJson.message)
+    })
+
+    return (
+        <>
+        </>
+    )
+}
+
+export default Notification
--- src/types/index.js
+++ src/types/index.js
@@ -10,7 +10,7 @@ else {
 const ACCESS_TOKEN_KEY_NAME = "ACCESS_TOKEN_JWT";
 const USER_KEY_NAME = "USER_INFO";
 
-const SOCKET_REGISTER_URL = `${BACKEND_URL}/ws/secured/messenger`;
+const SOCKET_REGISTER_URL = `${BACKEND_URL}/ws/secured`;
 const SOCKET_USER_TOPIC_PREFIX_URL = `/ws/secured/messenger/user`;
 
 export {
